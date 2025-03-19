86895debea1cec2d1424267e7276e74a65319aa8
eliassbt <elias.s.nascimento20@gmail.com>
2025-03-18 21:52:39 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,8 @@
+#Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+print ("ALUNO(A)          NOTA")
+print 
+print ("RAYSSA              9.0")  
+print ("ROBERTO              DEZ")
+print ("SHEYLA             4.5")    
+print ("CRYSTAL            7.0")
